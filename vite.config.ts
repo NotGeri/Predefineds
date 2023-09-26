f28d19e9769b573e12de69fde48df04926b861c2
NotGeri <46295973+NotGeri@users.noreply.github.com>
2023-09-26 06:29:51 +0200

    feat: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
     build: {
-      outDir: './dist'
+        outDir: './dist',
+        rollupOptions: {
+            external: ['/fontawesome.js']
+        }
     },
     plugins: [
         vue(),
